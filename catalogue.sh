4dbbdf577ed6713863523d457aaafc92f13ed003
sai123-sudo <ls0122207@gmail.com>
2025-05-26 13:51:26 +0530

    hello world created

--- catalogue.sh
+++ catalogue.sh
@@ -77,8 +77,6 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.vara84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -lt 0 ]
 then
     mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
